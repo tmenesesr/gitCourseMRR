a5047a249659bd99a4576954e6d9a5a1c59aa52e
Tirso <016xyz@gmail.com>
2023-02-01 17:54:51 -0300

    Se agregó la división en app.js

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ const restar = (x, y) => {
 const multiplicar = (x, y) => {
     return x * y
 }
+const dividir = (x, y) => {
+    if (y !== 0) {
+        return x / y
+    } else {
+        return 'No se puede dividir por cero'
+    }
+}    
